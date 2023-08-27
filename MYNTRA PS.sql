c960dd710f59a99d00b8e22cfb112d097893a2a6
Vishal Bagal <125578339+vsbagal@users.noreply.github.com>
2023-08-27 17:04:06 +0530

    Add files via upload

---
+++ MYNTRA PS.sql
@@ -0,0 +1,40 @@
+/*
+Easy Problem Statements:
+
+1. Retrieve User Information: Fetch the details of a specific user by providing their UserID.
+2. Total Orders: Calculate the total number of orders made by a given user.
+3. Product Availability: Check if a specific product is currently in stock.
+4. Recent Orders: Retrieve the latest 5 orders along with their order dates for a particular user.
+5. Product Count: Count the total number of products available in a certain category.
+6. Order Amount: Calculate the total amount spent by a user on a specific order.
+7. Popular Category: Find the category with the highest number of products.
+8. Average Price: Calculate the average price of products in a given category.
+9. Registration Month: Display the usernames and registration months of users who registered in the same month.
+10. Order Date Range: Retrieve orders made between a specified start and end date.
+
+Moderate Problem Statements:
+
+11. Top Buyers: List the top 5 users who have made the highest number of orders.
+12. Expensive Orders: Retrieve the details of orders with a total amount greater than a specified value.
+13. User Order History: Display the complete order history of a user, including product names, quantities, and order dates.
+14. Low Stock Products: List products that are currently low in stock (quantity less than 10).
+15. User Loyalty: Calculate the average time between consecutive orders for a specific user.
+16. Category Revenue: Determine the total revenue generated from a specific category's products.
+17. Inactive Users: Identify users who haven't placed any orders in the last 3 months.
+18. Order Item Count: Count the total number of items in each order and display the results.
+19. Product Recommendations: Suggest products to a user based on their previous order history.
+20. Frequent Buyers: Find users who have made more than 5 orders in the last 6 months.
+
+Advanced Problem Statements:
+
+21. Revenue Trend: Create a monthly revenue trend report for the last year, showing revenue for each month.
+22. User Churn Analysis: Identify users who haven't placed an order in the last 6 months and classify them as potential churn cases.
+23. Order Diversity: Determine the average number of different product categories in each order.
+24. Price Elasticity: Analyze how changes in product prices affect the quantity of products ordered.
+25. User Segmentation: Segment users into high, medium, and low spenders based on their total order amounts.
+26. Product Bundle Analysis: Identify products that are often purchased together in the same order.
+27. Inventory Management: Develop a query to help restock products when their quantity goes below a certain threshold.
+28. User Cohort Analysis: Study the buying behavior of users who registered in the same month over time.
+29. Product Return Rate: Calculate the return rate for each product category and highlight potential areas for improvement.
+30. Seasonal Patterns: Analyze whether certain product categories sell more during specific months of the year.
+*/
